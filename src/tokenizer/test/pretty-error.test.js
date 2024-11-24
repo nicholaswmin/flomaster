428713a5569b7026b33129452def45a792750d1b
Nicholas <nik.kyriakides@gmail.com>
2024-11-24 20:06:31 +0200

    test: cleanups

--- src/tokenizer/test/pretty-error.test.js
+++ src/tokenizer/test/pretty-error.test.js
@@ -1,10 +1,11 @@
 import test from 'node:test'
 import { PrettySyntaxError } from '../errors.js'
   
-const noAnsicode = str => str.replace(/\u001b\[.*?m/g, '')
-const noNewlines = str => str.trim().length > 0
-
 test('SyntaxError: pretty printing', async t => {    
+  const noAnsi = str => str.replace(/\u001b\[.*?m/g, '')
+  const hasLen = str => str.trim().length > 0
+  const format = str => str.split('\n').map(noAnsi).filter(hasLen).join('\n')
+
   const offset = 80
   const source = `
     LoremipsumdemipsumdolorLorem
@@ -14,34 +15,24 @@ test('SyntaxError: pretty printing', async t => {
     sumdolor
     `
 
+  let err = new PrettySyntaxError('Invalid token', { source, offset })
+
   await t.test('error message layout', async t => {    
     await t.test('visually points the source column', async t => {
-      try {
-        throw new PrettySyntaxError('Invalid token', { source, offset })
-      } catch ({ message }) {
-        t.assert.deepStrictEqual(message.split('\n')
-          .map(noAnsicode).filter(noNewlines), [
-            'Invalid token',
-            '    ⇩',
-            'dolo3rLomipsumdolor',
-            '    ⇧',
-            'Line: 3',
-            'Column: 4'
-          ])
-      }
+      t.assert.deepStrictEqual(format(err.message), [
+          'Invalid token',
+          '    ⇩',
+          'dolo3rLomipsumdolor',
+          '    ⇧',
+          'Line: 3',
+          'Column: 4'
+      ].join('\n'))
     })
   })
     
   await t.test('colors', async t => {
-    
     await t.test('is ANSI-code colored', async t => {
-      // 31m is ANSI CODE for red, if included, it's colored.
-      t.assert.throws(() => {
-        throw new PrettySyntaxError('Invalid token', { source, offset })
-      }, {
-        name: 'SyntaxError',
-        message: /31m/
-      })
+      t.assert.ok(err.message.includes('31m'))
     })
     
     await t.test('respects NO_COLOR/FORCE_COLOR env. variables', async t => {
@@ -56,39 +47,26 @@ test('SyntaxError: pretty printing', async t => {
       await t.test('NO_COLOR nor FORCE_COLOR is set', async t => {
         await t.test('is ANSI-code colored', async t => {
           // 31m is ANSI CODE for red, if included, it's colored.
-          t.assert.throws(() => {
-            throw new PrettySyntaxError('Invalid token', { source, offset })
-          }, {
-            name: 'SyntaxError',
-            message: /31m/
-          })
+          t.assert.ok(err.message.includes('31m'))
         })
       }) 
 
       await t.test('NO_COLOR is set', async t => {
-        await t.test('skips colors', async t => {    
-          process.env.NO_COLOR = '1'          
+        process.env.NO_COLOR = '1'          
+        const err = new PrettySyntaxError('Invalid token', { source, offset })
 
-          t.assert.throws(() => {
-            throw new PrettySyntaxError('Invalid token', { source, offset })
-          }, {
-            name: 'SyntaxError',
-            message: /dolo3rLomipsumdolor/
-          })
+        await t.test('skips colors', async t => {             
+          t.assert.ok(!err.message.includes('31m'))
         })
       })
       
       await t.test('both NO_COLOR and FORCE_COLOR are set', async t => {
+        process.env.FORCE_COLOR = '1'
+        process.env.NO_COLOR = '1'
+        const err = new PrettySyntaxError('Invalid token', { source, offset })
+
         await t.test('skips colors', async t => {    
-          process.env.FORCE_COLOR = '1'
-          process.env.NO_COLOR = '1'
-    
-          t.assert.throws(() => {
-            throw new PrettySyntaxError('Invalid token', { source, offset })
-          }, {
-            name: 'SyntaxError',
-            message: /dolo3rLomipsumdolor/
-          })
+          t.assert.ok(!err.message.includes('31m'))
         })
       })
     })
